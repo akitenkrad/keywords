40a733ddf579ae6210d8cf785e4fb677ed90100f
akitenkrad <akitenkrad@gmail.com>
2023-10-18 00:06:27 +0900

    update metrics.py

--- keywords/metrics.py
+++ keywords/metrics.py
@@ -19,8 +19,10 @@ class SpecializationFactor(object):
             ptn = keyword.get_keyword_ptn()
             count_all[keyword] = 0
             for text in texts:
-                count_all[keyword] += len([item for item in ptn.findall(text.lower()) if keyword.keyword in item])
-                count_total += len([item for item in ptn.findall(text.lower()) if keyword.keyword in item])
+                count_all[keyword] += len(
+                    [item for item in ptn.findall(text.lower()) if keyword.keyword.lower() in item]
+                )
+                count_total += len([item for item in ptn.findall(text.lower()) if keyword.keyword.lower() in item])
         for keyword in kws:
             count_all[keyword] = count_all[keyword] / (count_total + 1e-10)
 
@@ -32,9 +34,11 @@ class SpecializationFactor(object):
             for keyword in kws:
                 ptn = keyword.get_keyword_ptn()
                 count_by_text[idx][keyword] = len(
-                    [item for item in ptn.findall(text.lower()) if keyword.keyword in item]
+                    [item for item in ptn.findall(text.lower()) if keyword.keyword.lower() in item]
                 )
-                count_total_by_text += len([item for item in ptn.findall(text.lower()) if keyword.keyword in item])
+                count_total_by_text += len(
+                    [item for item in ptn.findall(text.lower()) if keyword.keyword.lower() in item]
+                )
             for keyword in kws:
                 count_by_text[idx][keyword] = count_by_text[idx][keyword] / (count_total_by_text + 1e-10)
 
