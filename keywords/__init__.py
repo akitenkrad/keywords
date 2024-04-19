f626c5e60eacf0bafd5ea9a4d4f2cc220342cea3
akitenkrad <akitenkrad@gmail.com>
2024-04-19 12:44:29 +0900

    update __init__.py

--- keywords/__init__.py
+++ keywords/__init__.py
@@ -1,2 +1,2 @@
-from .keywords import Keyword, KeywordCategory, extract_keywords
+from .keywords import Keyword, KeywordCategory, add_keywords_to_mecab_dic, add_noun_to_mecab_dict, extract_keywords
 from .metrics import SpecializationFactor, TfIdf
